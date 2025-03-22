1b586999a20e040671ee337c70867d95bcb26c5f
miteshp2110 <paliwalmitesh2110@gmail.com>
2025-03-22 16:30:49 +0530

    Added the redirection

--- app/src/main/java/com/xceptions/playlist/views/AuthActivity.kt
+++ app/src/main/java/com/xceptions/playlist/views/AuthActivity.kt
@@ -1,5 +1,6 @@
 package com.xceptions.playlist.views
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.ProgressBar
@@ -7,6 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.xceptions.playlist.databinding.ActivityAuthBinding
 import com.xceptions.playlist.utils.GoogleSignIn
+import com.xceptions.playlist.utils.SecurePrefManager
 
 class AuthActivity : AppCompatActivity() {
 
@@ -18,7 +20,18 @@ class AuthActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityAuthBinding.inflate(layoutInflater)
-
+        val token = SecurePrefManager.getJwtToken(this)
+        val isAdmin = SecurePrefManager.isAdmin(this)
+        if(token != null){
+            if(isAdmin){
+                startActivity(Intent(this,com.xceptions.playlist.views.admin.AdminActivity::class.java))
+                finish()
+                return
+            }
+            startActivity(Intent(this,com.xceptions.playlist.views.user.UserActivity::class.java))
+            finish()
+            return
+        }
 
         setContentView(binding.root)
 
