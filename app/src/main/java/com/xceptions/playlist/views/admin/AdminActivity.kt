ff2dd66e9f9473629056778ac2feab2bf552f276
miteshp2110 <paliwalmitesh2110@gmail.com>
2025-03-24 23:02:37 +0530

    Added bottom nav for admin

--- app/src/main/java/com/xceptions/playlist/views/admin/AdminActivity.kt
+++ app/src/main/java/com/xceptions/playlist/views/admin/AdminActivity.kt
@@ -1,14 +1,21 @@
 package com.xceptions.playlist.views.admin
 
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.xceptions.playlist.R
+import com.xceptions.playlist.databinding.ActivityAdminBinding
 
 class AdminActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAdminBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_admin)
+        binding = ActivityAdminBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
     }
 }
